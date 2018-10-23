681f3ab2ca01217c2fb64adef5f60371e4fe7325
DESKTOP-9U92OMC\fache <b.fache@ludus-academie.com>
2018-10-23 20:29:17 +0200

    Version 2 du Programme "nombre plus Grand ?" avec des corrections dans la valeur d'initilisation de la variable max rendant le programme fonctionelle. Correction de l'affichage utilisateur

--- PlusGrand/main.c
+++ PlusGrand/main.c
@@ -9,17 +9,17 @@ SORTIE Le plus grand
 #define NOMBRE 3
 int main()
 {
-   int nMax=(-2147843647),nNombre=0,nCpt;
+   int nMax=(-2147483648),nNombre=0,nCpt;
 
     for(nCpt=1;nCpt<=NOMBRE;nCpt++){
-        printf("Quelle est le premier nombre ?\n");
+        printf("Quelle est le nombre n%i ?\n",nCpt);
         scanf("%i",&nNombre);
         if(nNombre>nMax){
             nMax = nNombre;
         }
     }
 
-    printf("Le plus grand est : %i ?\n",nMax);
+    printf("Le plus grand est : %i .\n",nMax);
 
     return 0;
 }
