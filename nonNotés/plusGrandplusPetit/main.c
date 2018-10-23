d6e320fe30759d8890d55174d0d393d44d5bdc0f
DESKTOP-9U92OMC\fache <b.fache@ludus-academie.com>
2018-10-23 20:54:19 +0200

    Version 2 de plusGrandplusPetit : Correction du programme pour permettre de saisir plus de 3 nombres, 0 affiche le résultat.

--- nonNotés/plusGrandplusPetit/main.c
+++ nonNotés/plusGrandplusPetit/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
-BUT Ecrire un programme qui affiiche le plus grand et le plus petit de 3 entiers saisies.
+BUT Ecrire un programme qui affiiche le plus grand et le plus petit de nombres entiers saisie. Le programme se termine si on tape 0.
 ENTREE Les 3 Nombre
 SORTIE Le plus grand et le plus petit
 
@@ -9,10 +9,11 @@ SORTIE Le plus grand et le plus petit
 #define NOMBRE 3
 int main()
 {
-   int nMax=(-2147483648),nMin=(2147483647),nNombre=0,nCpt;
+   int nMax=(-2147483648),nMin=(2147483647),nNombre=-2147483648,nCpt=1;
 
-    for(nCpt=1;nCpt<=NOMBRE;nCpt++){
-        printf("Quelle est le nombre n%i ?\n",nCpt);
+
+    while(nNombre!=0){
+        printf("Quelle est le nombre n%i ? Taper 0 pour arreter.\n",nCpt);
         scanf("%i",&nNombre);
         if(nNombre>nMax){
             nMax = nNombre;
@@ -20,6 +21,7 @@ int main()
         if(nNombre<nMin){
             nMin = nNombre;
         }
+        nCpt++;
     }
 
     printf("Le plus grand est : %i .\n",nMax);
